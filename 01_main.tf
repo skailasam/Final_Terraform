66ae6491c3a10caf570366e657abc37143dccae9
yoen1905 <yn1905@naver.com>
2021-11-18 09:28:41 +0900

    Edit

--- 01_main.tf
+++ 01_main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "${var.region}"
-    access_key = "AKIA4Z6OBOAQBJ6YEOL5"
-    secret_key = "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"
+    access_key = "access_key"
+    secret_key = "secret_key"
 }
 
 
